509218bcd6808d7577b48bd40130310764aa03a4
bravint <bravint@gmail.com>
2022-02-14 15:49:55 +0000

    fix async function for pw comparison

--- src/router.js
+++ src/router.js
@@ -12,38 +12,38 @@ const generateHashedPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
 
     return await bcrypt.hash(password, salt);
-}
+};
 
 router.post('/register', async (req, res) => {
-    let {username, password} = req.body
+    let { username, password } = req.body;
 
-    password = await generateHashedPassword(password)
+    password = await generateHashedPassword(password);
 
-    const user = {username, password}
+    const user = { username, password };
 
     const createdUser = await prisma.user.create({
-        data: user
-    })
+        data: user,
+    });
 
-    res.json(createdUser)
+    res.json(createdUser);
 });
 
 router.post('/login', async (req, res) => {
-    let {username, password} = req.body
+    let { username, password } = req.body;
 
     const findUserbyUsermame = await prisma.user.findUnique({
         where: {
-            username
-        }
-    })
+            username,
+        },
+    });
 
-    if (!findUserbyUsermame) res.status(404).send(`no user found with username ${username}`)
+    if (!findUserbyUsermame) res.status(404).send(`no user found with username ${username}`);
 
-    if (bcrypt.compare(password, findUserbyUsermame.password)) {
-        return res.json(jwt.sign(username, secret))
+    if (await bcrypt.compare(password, findUserbyUsermame.password)) {
+        return res.json(jwt.sign(username, secret));
     }
 
-    res.status(401).send('incorrect credentials provided')
+    res.status(401).json('incorrect credentials provided');
 });
 
 module.exports = router;
